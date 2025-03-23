b0a79ba2b7ec3baacd60e180df2e49868ee464f1
Pieter Immanuel Sinaga <pieterimmanuel04@gmail.com>
2025-03-23 20:31:14 +0700

    menambahkan class SayaTubeVideo

---
+++ tpmodul6_103022330111/SayaTubeVideo.cs
@@ -0,0 +1,40 @@
+﻿class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playcount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random random = new Random();
+        this.id = random.Next(10000, 99999);
+        this.title = title;
+        this.playcount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        if(count < 0)
+        {
+            Console.WriteLine("Jumlah tidak boleh negatif");
+        }
+        this.playcount += count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("ID: " + this.id);
+        Console.WriteLine("Title: " + this.title);
+        Console.WriteLine("PlayCount: " + this.playcount);
+    }
+}
+
+class program
+{
+    static void Main()
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design by Contract - [Pieter Immanuel Sina]");
+        video.IncreasePlayCount(10);
+        video.PrintVideoDetails();
+    }
+}
--- tpmodul6_103022330111/obj/Debug/net8.0/tpmodul6_103022330111.AssemblyInfo.cs
+++ tpmodul6_103022330111/obj/Debug/net8.0/tpmodul6_103022330111.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul6_103022330111")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43fd0cc75fcd8258662f393ac1e2e966ff9c5b24")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul6_103022330111")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul6_103022330111")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
